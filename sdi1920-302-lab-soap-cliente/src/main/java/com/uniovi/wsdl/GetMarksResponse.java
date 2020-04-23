e0aa27672a79d1b19f62b10dae9790e38d631e44
Pablo Cañal Suárez <uo264903@uniovi.es>
2020-04-23 22:01:28 +0200

    SDI-SW-11.2 Servicio SOAP Cliente

--- sdi1920-302-lab-soap-cliente/src/main/java/com/uniovi/wsdl/GetMarksResponse.java
+++ sdi1920-302-lab-soap-cliente/src/main/java/com/uniovi/wsdl/GetMarksResponse.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.0 
 // Visite <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2020.04.23 a las 10:00:34 PM CEST 
+// Generado el: 2020.04.23 a las 10:00:57 PM CEST 
 //
 
 
